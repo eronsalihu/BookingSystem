a7b9559677af9ea7906f1efe49103229aaf6034e
Eduarto Shurdha <eduarto.shurdha@atis.al>
2022-10-13 14:51:24 +0200

    merge

--- BookingSystem/Controllers/RoomController.cs
+++ BookingSystem/Controllers/RoomController.cs
@@ -7,84 +7,84 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookingSystem.Controllers
 {
-    public class RoomController : BaseApiController
-    {
-        private readonly IRoomService _roomService;
+	public class RoomController : BaseApiController
+	{
+		private readonly IRoomService _roomService;
 
-        public RoomController(IRoomService roomService)
-        {
-            _roomService = roomService;
-        }
+		public RoomController(IRoomService roomService)
+		{
+			_roomService = roomService;
+		}
 
-        [HttpGet("roomsByGuestHouseId")]
-        public async Task<IActionResult> GetRooms(int id)
-        {
-            return Ok(_roomService.GetRoomsByGuestHouseId(id).Result);
-        }
-
-
-        [HttpGet("roomById")]
-        public async Task<RoomDto> GetRoomById(int id)
-        {
-            return _roomService.GetRoomById(id);
-        }
-
-        [HttpPost("room")]
-        public async Task<IActionResult> AddRoom([FromBody] List<RoomDto> roomDtos)
-        {
-            var roomList = new List<Room>();
-            foreach (var roomDto in roomDtos)
-            {
-                var room = new Room
-                {
-                    Name = roomDto.Name,
-                    Description = roomDto.Description,
-                    Image = roomDto.Image,
-                    Price = roomDto.Price,
-                    NumberOfBeds = roomDto.NumberOfBeds,
-                    CreatedBy = GetCurrentUser(),
-                    GuestHouseId = roomDto.GuestHouseId,
-                    Amenities = roomDto.Amenities.Select(e => new RoomAmenity
-                    {
-                        Amenities = e
-                    }).ToList() ?? null,
-
-                };
-                roomList.Add(room);
-            }
-
-            return Ok(await _roomService.AddRoomsAsync(roomList));
-        }
-
-        [HttpPut("room")]
-        public IActionResult UpdateRoom(int id, [FromBody] RoomDto roomDto)
-        {
-            var room = new Room
-            {
-                Id = id,
-                Name = roomDto.Name,
-                Image = roomDto.Image,
-                Description = roomDto.Description,
-                Price = roomDto.Price,
-                NumberOfBeds = roomDto.NumberOfBeds,
-                CreatedBy = GetCurrentUser(),
-                GuestHouseId = roomDto.GuestHouseId,
-                Amenities = roomDto.Amenities.Select(e => new RoomAmenity
-                {
-                    RoomId = id,
-                    Amenities = e
-                }).ToList() ?? null,
-            };
-            var updatedRoom = _roomService.UpdateRoomAsync(room);
-            if (updatedRoom.Result == null)
-                return BadRequest(new ApiException(404, "Room not found"));
-
-            return Ok(updatedRoom.Result);
-        }
+		[HttpGet("GuestHouse/{id}")]
+		public async Task<IActionResult> GetRooms(int id)
+		{
+			return Ok(_roomService.GetRoomsByGuestHouseId(id).Result);
+		}
 
 
-        [HttpDelete("room")]
-        public void DeleteRoom(int id) =>
-             _roomService.DeleteRoomAsync(id);
-    }
+		[HttpGet("{id}")]
+		public async Task<RoomDto> GetRoomById(int id)
+		{
+			return _roomService.GetRoomById(id);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddRoom([FromBody] List<RoomDto> roomDtos)
+		{
+			var roomList = new List<Room>();
+			foreach (var roomDto in roomDtos)
+			{
+				var room = new Room
+				{
+					Name = roomDto.Name,
+					Description = roomDto.Description,
+					Image = roomDto.Image,
+					Price = roomDto.Price,
+					NumberOfBeds = roomDto.NumberOfBeds,
+					CreatedBy = GetCurrentUser(),
+					GuestHouseId = roomDto.GuestHouseId,
+					Amenities = roomDto.Amenities.Select(e => new RoomAmenity
+					{
+						Amenities = e
+					}).ToList() ?? null,
+
+				};
+				roomList.Add(room);
+			}
+
+			return Ok(await _roomService.AddRoomsAsync(roomList));
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult UpdateRoom(int id, [FromBody] RoomDto roomDto)
+		{
+			var room = new Room
+			{
+				Id = id,
+				Name = roomDto.Name,
+				Image = roomDto.Image,
+				Description = roomDto.Description,
+				Price = roomDto.Price,
+				NumberOfBeds = roomDto.NumberOfBeds,
+				CreatedBy = GetCurrentUser(),
+				GuestHouseId = roomDto.GuestHouseId,
+				Amenities = roomDto.Amenities.Select(e => new RoomAmenity
+				{
+					RoomId = id,
+					Amenities = e
+				}).ToList() ?? null,
+			};
+			var updatedRoom = _roomService.UpdateRoomAsync(room);
+			if (updatedRoom.Result == null)
+				return BadRequest(new ApiException(404, "Room not found"));
+
+			return Ok(updatedRoom.Result);
+		}
+
+
+		[HttpDelete("room")]
+		public void DeleteRoom(int id) =>
+			 _roomService.DeleteRoomAsync(id);
+	}
 }
--- BookingSystem/Dtos/RoomDto.cs
+++ BookingSystem/Dtos/RoomDto.cs
@@ -3,28 +3,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BookingSystem.Dtos
 {
-    public class RoomDto
-    {
-        public int Id { get; set; }
+	public class RoomDto
+	{
+		public int Id { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+		[Required]
+		public string Name { get; set; }
 
-        [Required]
-        public string Description { get; set; }
+		[Required]
+		public string Description { get; set; }
 
-        [Required]
-        public byte[] Image { get; set; }
+		[Required]
+		public byte[] Image { get; set; }
 
-        [Required]
-        public decimal Price { get; set; } 
+		[Required]
+		public decimal Price { get; set; }
 
-        [Required]
-        public int NumberOfBeds { get; set; }
+		[Required]
+		public int NumberOfBeds { get; set; }
 
-        [Required]
-        public int GuestHouseId { get; set; }
+		[Required]
+		public int GuestHouseId { get; set; }
 
-        public List<AmenitiesEnum> Amenities { get; set; } = new List<AmenitiesEnum>();
-    }
+		public List<AmenitiesEnum> Amenities { get; set; } = new List<AmenitiesEnum>();
+	}
 }
