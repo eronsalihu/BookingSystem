d919c52b0d0ea79c1d241ac54294fe52a985fa53
eronsalihu <eron821@gmail.com>
2022-10-17 10:29:36 +0200

    small fixes

--- BookingSystem/Controllers/RoomController.cs
+++ BookingSystem/Controllers/RoomController.cs
@@ -7,77 +7,83 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookingSystem.Controllers
 {
-	public class RoomController : BaseApiController
-	{
-		private readonly IRoomService _roomService;
+    public class RoomController : BaseApiController
+    {
+        private readonly IRoomService _roomService;
 
-		public RoomController(IRoomService roomService)
-		{
-			_roomService = roomService;
-		}
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
 
-		[HttpGet("GuestHouse/{id}")]
-		public async Task<IActionResult> GetRooms(int id)
-		{
-			return Ok(_roomService.GetRoomsByGuestHouseId(id).Result);
-		}
+        [HttpGet]
+        public async Task<IActionResult> GetRooms()
+        {
+            return Ok(await _roomService.GetRoomsAsync());
+        }
 
-		[HttpGet("{id}")]
-		public async Task<RoomDto> GetRoomById(int id)
-		{
-			return _roomService.GetRoomById(id);
-		}
+        [HttpGet("GuestHouse/{id}")]
+        public async Task<IActionResult> GetRoomsByGuestHouseId(int id)
+        {
+            return Ok(_roomService.GetRoomsByGuestHouseId(id).Result);
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> AddRoom([FromBody] RoomDto roomDto)
-		{
-			var room = new Room
-			{
-				Name = roomDto.Name,
-				Description = roomDto.Description,
-				Image = roomDto.Image,
-				Price = roomDto.Price,
-				NumberOfBeds = roomDto.NumberOfBeds,
-				CreatedBy = GetCurrentUser(),
-				GuestHouseId = roomDto.GuestHouseId,
-				Amenities = roomDto.Amenities.Select(e => new RoomAmenity
-				{
-					Amenities = e
-				}).ToList() ?? null,
+        [HttpGet("{id}")]
+        public async Task<RoomDto> GetRoomById(int id)
+        {
+            return _roomService.GetRoomById(id);
+        }
 
-			};
+        [HttpPost]
+        public async Task<IActionResult> AddRoom([FromBody] RoomDto roomDto)
+        {
+            var room = new Room
+            {
+                Name = roomDto.Name,
+                Description = roomDto.Description,
+                Image = roomDto.Image,
+                Price = roomDto.Price,
+                NumberOfBeds = roomDto.NumberOfBeds,
+                CreatedBy = GetCurrentUser(),
+                GuestHouseId = roomDto.GuestHouseId,
+                Amenities = roomDto.Amenities.Select(e => new RoomAmenity
+                {
+                    Amenities = e
+                }).ToList() ?? null,
 
-			return Ok(await _roomService.AddRoomsAsync(room));
-		}
+            };
 
-		[HttpPut("{id}")]
-		public IActionResult UpdateRoom(int id, [FromBody] RoomDto roomDto)
-		{
-			var room = new Room
-			{
-				Id = id,
-				Name = roomDto.Name,
-				Image = roomDto.Image,
-				Description = roomDto.Description,
-				Price = roomDto.Price,
-				NumberOfBeds = roomDto.NumberOfBeds,
-				CreatedBy = GetCurrentUser(),
-				GuestHouseId = roomDto.GuestHouseId,
-				Amenities = roomDto.Amenities.Select(e => new RoomAmenity
-				{
-					RoomId = id,
-					Amenities = e
-				}).ToList() ?? null,
-			};
-			var updatedRoom = _roomService.UpdateRoomAsync(room);
-			if (updatedRoom.Result == null)
-				return BadRequest(new ApiException(404, "Room not found"));
+            return Ok(await _roomService.AddRoomsAsync(room));
+        }
 
-			return Ok(updatedRoom.Result);
-		}
+        [HttpPut("{id}")]
+        public IActionResult UpdateRoom(int id, [FromBody] RoomDto roomDto)
+        {
+            var room = new Room
+            {
+                Id = id,
+                Name = roomDto.Name,
+                Image = roomDto.Image,
+                Description = roomDto.Description,
+                Price = roomDto.Price,
+                NumberOfBeds = roomDto.NumberOfBeds,
+                CreatedBy = GetCurrentUser(),
+                GuestHouseId = roomDto.GuestHouseId,
+                Amenities = roomDto.Amenities.Select(e => new RoomAmenity
+                {
+                    RoomId = id,
+                    Amenities = e
+                }).ToList() ?? null,
+            };
+            var updatedRoom = _roomService.UpdateRoomAsync(room);
+            if (updatedRoom.Result == null)
+                return BadRequest(new ApiException(404, "Room not found"));
 
-		[HttpDelete("{id}")]
-		public void DeleteRoom(int id) =>
-			 _roomService.DeleteRoomAsync(id);
-	}
+            return Ok(updatedRoom.Result);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteRoom(int id) =>
+             _roomService.DeleteRoomAsync(id);
+    }
 }
--- BookingSystem/Interfaces/IRoomService.cs
+++ BookingSystem/Interfaces/IRoomService.cs
@@ -3,12 +3,13 @@ using BookingSystem.Entities;
 
 namespace BookingSystem.Interfaces
 {
-	public interface IRoomService
-	{
-		Task<List<RoomDto>> GetRoomsByGuestHouseId(int guestHouseId);
-		RoomDto GetRoomById(int id);
-		Task<RoomDto> AddRoomsAsync(Room rooms);
-		Task<RoomDto> UpdateRoomAsync(Room room);
-		void DeleteRoomAsync(int id);
-	}
+    public interface IRoomService
+    {
+        Task<List<RoomDto>> GetRoomsAsync();
+        Task<List<RoomDto>> GetRoomsByGuestHouseId(int guestHouseId);
+        RoomDto GetRoomById(int id);
+        Task<RoomDto> AddRoomsAsync(Room rooms);
+        Task<RoomDto> UpdateRoomAsync(Room room);
+        void DeleteRoomAsync(int id);
+    }
 }
